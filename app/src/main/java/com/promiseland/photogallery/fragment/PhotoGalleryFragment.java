0feb8c2452a8d7f3e2716a3eef154f051cc4cce6
wangmingming <wangmingming@fly-see.com>
2016-07-28 15:02:55 +0800

    update to asynctask init

--- app/src/main/java/com/promiseland/photogallery/fragment/PhotoGalleryFragment.java
+++ app/src/main/java/com/promiseland/photogallery/fragment/PhotoGalleryFragment.java
@@ -7,10 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
 import com.promiseland.photogallery.R;
+import com.promiseland.photogallery.bean.GalleryItem;
 import com.promiseland.photogallery.utils.FlickrFetchr;
+
+import java.util.ArrayList;
 
 /**
  * Created by joseph on 2016/7/26.
@@ -19,6 +23,7 @@ public class PhotoGalleryFragment extends Fragment {
     private static final String TAG = "PhotoGalleryFragment";
 
     private GridView mGridView;
+    private ArrayList<GalleryItem> mItems;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,12 +41,34 @@ public class PhotoGalleryFragment extends Fragment {
         return rootView;
     }
 
-    private class FetchItemsTask extends AsyncTask<Void, Void, Void>{
+    private void setupAdapter(){
+        if(getActivity() == null || mGridView == null){
+            return;
+        }
+        if(mItems != null){
+            mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(), android.R.layout.simple_gallery_item, mItems));
+        } else {
+            mGridView.setAdapter(null);
+        }
+    }
+
+    private class FetchItemsTask extends AsyncTask<String, Integer, ArrayList<GalleryItem>>{// 输入类型，进度更新类型，返回值类型
 
         @Override
-        protected Void doInBackground(Void... voids) {
-            new FlickrFetchr().fetchItems();
-            return null;
+        protected ArrayList<GalleryItem> doInBackground(String... params) {
+            ArrayList<GalleryItem> items = new FlickrFetchr().fetchItems();
+            return items;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<GalleryItem> galleryItems) {
+            mItems = galleryItems;
+            setupAdapter();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
         }
     }
 }
--- app/src/main/java/com/promiseland/photogallery/utils/FlickrFetchr.java
+++ app/src/main/java/com/promiseland/photogallery/utils/FlickrFetchr.java
@@ -3,6 +3,8 @@ package com.promiseland.photogallery.utils;
 import android.net.Uri;
 import android.util.Log;
 
+import com.promiseland.photogallery.bean.GalleryItem;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -13,11 +15,12 @@ import java.io.InputStream;
 import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class FlickrFetchr {
     public static final String TAG = "PhotoFetcher";
 
-    private static final String ENDPOINT = "http://api.flickr.com/services/rest/";
+    private static final String ENDPOINT = "https://api.flickr.com/services/rest/";
     private static final String API_KEY = "2e40ba91b199589487862403593e4eb1";
     private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
     private static final String PARAM_EXTRAS = "extras";
@@ -51,11 +54,13 @@ public class FlickrFetchr {
         }
     }
 
-    String getUrl(String urlSpec) throws IOException {
+    public String getUrl(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
     }
-    
-    public void fetchItems() {
+
+    public ArrayList<GalleryItem> fetchItems() {
+        ArrayList<GalleryItem> items = new ArrayList<GalleryItem>();
+
         try {
             String url = Uri.parse(ENDPOINT).buildUpon()
                     .appendQueryParameter("method", METHOD_GET_RECENT)
@@ -68,31 +73,33 @@ public class FlickrFetchr {
             XmlPullParser parser = factory.newPullParser();
             parser.setInput(new StringReader(xmlString));
 
+            parseItems(items, parser);
         } catch (IOException ioe) {
             Log.e(TAG, "Failed to fetch items", ioe);
         } catch (XmlPullParserException xppe) {
             Log.e(TAG, "Failed to parse items", xppe);
         }
+        return items;
     }
 
-//    void parseItems(ArrayList<GalleryItem> items, XmlPullParser parser) throws XmlPullParserException, IOException {
-//        int eventType = parser.next();
-//
-//        while (eventType != XmlPullParser.END_DOCUMENT) {
-//            if (eventType == XmlPullParser.START_TAG &&
-//                XML_PHOTO.equals(parser.getName())) {
-//                String id = parser.getAttributeValue(null, "id");
-//                String caption = parser.getAttributeValue(null, "title");
-//                String smallUrl = parser.getAttributeValue(null, EXTRA_SMALL_URL);
-//
-//                GalleryItem item = new GalleryItem();
-//                item.setId(id);
-//                item.setCaption(caption);
-//                item.setUrl(smallUrl);
-//                items.add(item);
-//            }
-//
-//            eventType = parser.next();
-//        }
-//    }
+    private void parseItems(ArrayList<GalleryItem> items, XmlPullParser parser) throws XmlPullParserException, IOException {
+        int eventType = parser.next();
+
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            if (eventType == XmlPullParser.START_TAG &&
+                XML_PHOTO.equals(parser.getName())) {
+                String id = parser.getAttributeValue(null, "id");
+                String caption = parser.getAttributeValue(null, "title");
+                String smallUrl = parser.getAttributeValue(null, EXTRA_SMALL_URL);
+
+                GalleryItem item = new GalleryItem();
+                item.setId(id);
+                item.setCaption(caption);
+                item.setUrl(smallUrl);
+                items.add(item);
+            }
+
+            eventType = parser.next();
+        }
+    }
 }
